eeb7ceced642de6bf42667dea21a23b0765064f5
João Victor Bortolozo <110576032+joaovictorbortolozo@users.noreply.github.com>
2022-11-10 22:28:25 -0300

    exercícios

---
+++ 002/app.js
@@ -0,0 +1,4 @@
+function Exibir(){
+ let valor1 = document.getElementById('n1').ariaValueMax
+ document.getElementById('Resposta').innerHTML = 'O número informado foi' + valor1 + '.'
+}
---
+++ 005/app.js
@@ -0,0 +1,8 @@
+function Converter(){
+    let valor1 = document.getElementById('n1')
+
+    let total = parseFloat(valor1)/100
+
+    document.getElementById('resposta').innerHTML=total/100
+    
+}
